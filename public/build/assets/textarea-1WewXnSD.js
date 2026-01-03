b1b77b01f926872fd6ce9757df0c6bb2d5d4abfc
Andresillo <you@example.com>
2026-01-03 15:18:14 -0300

    chore: generate new build assets.

---
+++ public/build/assets/textarea-1WewXnSD.js
@@ -0,0 +1,6 @@
+import{c as l,a as n}from"./button-BUox-IYX.js";import{r as c,c as d,j as p}from"./app-f77woEWo.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const f=[["path",{d:"M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z",key:"1ffxy3"}],["path",{d:"m21.854 2.147-10.94 10.939",key:"12cjpa"}]],b=l("Send",f),u=c.forwardRef((r,i)=>{const e=d.c(9);let s,o;e[0]!==r?({className:s,...o}=r,e[0]=r,e[1]=s,e[2]=o):(s=e[1],o=e[2]);let a;e[3]!==s?(a=n("flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",s),e[3]=s,e[4]=a):a=e[4];let t;return e[5]!==o||e[6]!==i||e[7]!==a?(t=p.jsx("textarea",{className:a,ref:i,...o}),e[5]=o,e[6]=i,e[7]=a,e[8]=t):t=e[8],t});u.displayName="Textarea";export{b as S,u as T};
